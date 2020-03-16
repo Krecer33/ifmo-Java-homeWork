fefc6caa67175ca0eaae59194a8a151617ec40ce
KostyaK <ostrov91@mail.ru>
2020-03-16 15:12:04 +0300

    Поправил ошибки в 5-ой задаче

--- src/homework1/Task5.java
+++ src/homework1/Task5.java
@@ -5,12 +5,19 @@ import java.util.Arrays;
 public class Task5 {
     public static void main(String[] args) {
         int[][] newArr = new int[5][8];
-        for (int i = 0; i < newArr.length; i++) {
+        for (int i = 0; i < newArr.length; i++) { // цикл заполнения элементов массива рандомными числами  в диапозоне[-99;99]
             for (int j = 0; j < newArr[i].length; j++) {
                 newArr[i][j] = ((int) (Math.random() * 199) - 99);
+            }}
+        int max = newArr[0][0];
+        for(int i =0;i< newArr.length;i++){    // цикл сравнения элементов
+            for(int j=0; j < newArr[i].length; j++){
+                if(newArr[i][j] > max) max = newArr[i][j];
             }
-            System.out.println(Arrays.deepToString(newArr));
+        }
+        System.out.println(Arrays.deepToString(newArr));
+        System.out.println(" ");
+        System.out.println("Значение максимального элемента массива = " + max);
 
         }
     }
-}
